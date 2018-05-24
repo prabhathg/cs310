e6e83f3e73f0489df18ec97bb0e6fc41e283690e
prabhathg <prabhathg@gmail.com>
2018-05-24 15:52:39 +0530

    Create MainWindow.java

---
+++ MainWindow.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+  public MainWindow() {
+	init();
+  }	
+
+  public void init() {
+	this.setTitle("CS310 Window");
+	this.setSize(400, 400);
+	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+  public static void main(String[] args) {
+     	MainWindow mw = new MainWindow();
+	mw.setVisible(true); 
+  }
+}
