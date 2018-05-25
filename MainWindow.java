362f0f25d17ae4ff3866387d4119dd1423c20315
charith9422 <35757692+charith9422@users.noreply.github.com>
2018-05-25 15:34:31 +0530

    Name changed

--- MainWindow.java
+++ MainWindow.java
@@ -7,13 +7,12 @@ public class MainWindow extends JFrame {
   }	
 
   public void init() {
-<<<<<<< .mine
+
 	this.setTitle("CS310-Cjkbhr");
 
-=======
+
 	this.setTitle("CS310 pramuka weerasinghe");
 	this.setTitle("CS310-Chr");
->>>>>>> .theirs
 	this.setSize(600, 400);
 	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
